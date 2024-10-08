9b4eaac8364c9fea017a9295938f391d22127ae6
SoohoPark <clapp301@gmail.com>
2024-10-08 23:02:19 +0900

    Stack_Queue Q3

--- Stack_and_Queue/Q3_C_SQ.c
+++ Stack_and_Queue/Q3_C_SQ.c
@@ -103,7 +103,23 @@ int main()
 
 int isStackPairwiseConsecutive(Stack *s)
 {
-  /* add your code here */
+	if(s->ll.size % 2 != 0)
+		return 0;
+
+	int prev, cur;
+
+	while(!isEmptyStack(s)) {
+		prev = pop(s);
+		cur = pop(s);
+
+		if(abs(cur - prev) == 1) 
+			continue;
+		else {
+			return 0;
+		}
+	}
+
+	return 1;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
